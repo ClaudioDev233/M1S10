a068c0b54abd70f806e74d0dc6ad82f564558177
Claudio <claudio.junior233@gmail.com>
2022-01-14 16:19:11 -0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router';
 import './App.css';
 
+import Cart from './components/pages/cart';
+import Details from './components/pages/details';
+import Home from './components/pages/home';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return  <>
+  <p>Hello</p>
+
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/cart" element={<Cart/>}/>
+    <Route path="/details/:id" element={<Details/>}/>
+  </Routes>
+  
+  </>
 }
 
 export default App;
