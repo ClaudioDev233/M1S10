1d08fc01ed72d156ea6da16e0fbcad38368a087c
Claudio <claudio.junior233@gmail.com>
2022-01-17 15:32:51 -0300

    added params the right way

--- src/components/booksDetails/index.js
+++ src/components/booksDetails/index.js
@@ -1,17 +1,26 @@
 import { useParams } from "react-router"
-
+import { useState, useEffect} from "react"
 export default function BookDetails(){
+
+    const [bookData, setBookData] = useState([])
 
     const params = useParams()
      
-    const handleDetails = async () => {
-        const resp = await fetch (`http://localhost:3333/books/?${params.id}`)
-    }
 
+    useEffect(() =>{
+        const handleDetails = async () => {
+            const resp = await fetch (`http://localhost:3333/books/${params.id}`)
+            const data = await resp.json()
+            console.log(data)
+            setBookData(data)
+        }
+        handleDetails()
+    }, [params.id])
+    
     return <>
     <div className="details__container">
         <div className="details__image">
-        IMAGEM
+        <img src={bookData.image}/>
         </div>
         <div className="details__content">
             <h2 className="details__name">Nome</h2>
